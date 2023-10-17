e18752ba7ae43d1ea996237c20e1de6ee8064749
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-10-17 09:33:57 -0700

    deleted unnecessary files

--- test/main.h
+++ test/main.h
@@ -6,24 +6,8 @@
 #include <unistd.h>
 #include <limits.h>
 
-/**
- * struct format - the format specifier and its function
- * @fs: the format specifier
- * @f: the function associtated
- */
-
-typedef struct format
-{
-	char *fs;
-	int (*f)();
-} formsp;
-
 int _strlen(char *string);
 int _putchar(char c);
-int print_c(va_list list);
-int print_s(va_list list);
-int print_p(void);
-int print_i(va_list list);
 int _puts(char *);
 int print_int(int);
 int print_ui(unsigned int);
@@ -38,7 +22,6 @@ int print_pthex(unsigned long int);
 int rev_print(char *);
 int get_rotated(char *);
 
-int get_spec(char format, va_list list, int buff_i);
 int _printf(const char *format, ...);
 
 #endif
